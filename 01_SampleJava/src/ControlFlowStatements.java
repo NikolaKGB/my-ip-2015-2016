e9e0ce1ba27c608b5f149969505b12b09cfd035f
NikolaKGB <nikola.marinoff@gmail.com>
2015-10-14 08:26:10 +0300

    controlflowstatements

---
+++ 01_SampleJava/src/ControlFlowStatements.java
@@ -0,0 +1,28 @@
+public class ControlFlowStatements {
+
+	public static void main(String[] args) {
+		
+		forExample();
+	}
+
+	private static void forExample() {
+		for (int i = 0; i < 10; i++) {
+			System.out.println(i);
+		}
+	}
+
+	private static void ifExample() {
+
+		if (true) {
+			System.out.println("called");
+		}
+		
+
+		if (1>2) {
+			System.out.println("not called");
+		} else {
+			System.out.println("called");
+		}
+	}
+
+}
